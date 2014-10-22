ce2e1f0eda00b8c0da8c87bed6db215b24c33606
seemajune <seemashariat@gmail.com>
2014-10-22 14:28:15 -0400

    7 more

--- models/rps_game.rb
+++ models/rps_game.rb
@@ -1,2 +1,52 @@
 class RPSGame
+
+
+  attr_accessor :play
+  attr_reader :computer_play
+
+  SCORES = {:scissors => "1", :rock => "2", :paper => "3"}
+
+  def initialize(play)
+    @play = play
+
+  end
+
+  class PlayTypeError < StandardError
+    def raise_error
+      "an error"
+    end
+  end
+
+  def computer_play
+    SCORES.keys.at( rand(3) )
+  end
+
+
+  def self.valid_play?(play)
+    if play == :rock || play == :paper || play == :scissors
+      true
+    else
+      PlayTypeError.new
+    end
+  end
+
+  def tied?
+    @play == @computer_play
+  end
+
+  def won?
+    if (@computer_play == :rock && @play == :paper) || (@computer_play == :scissors && @play == :rock) || (@computer_play == :paper && @play == :scissors)
+      true
+    end
+  end
+
+  def lost?
+    if !(won?)
+      true
+    end
+  end
+
+
 end
+
+
